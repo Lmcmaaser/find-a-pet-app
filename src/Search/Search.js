9f1cdb5ac2dfcc21336ba7cb5bc2832d97cd5bc6
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2020-04-24 19:51:26 -0700

    edited Search page

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -8,47 +8,9 @@ import './Search.css';
 //add a show results component
 
 class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pets: this.props.data.pets,
-      types: this.props.data.types
-    }
-  };
-  /*constructor(props) {
-      super(props);
-      this.handleInput = this.handleInput.bind(this);
-      this.state = {
-        query: '',
-        results: []
-      }
-  }*/
-
-  static defaultProps = {
-    match: {
-      params: {}
-    }
-  }
   static contextType = PetContext;
-
-  /*handleInput(title) {
-      console.log("handleInput fired")
-      console.log(title) //correctly passes input
-      this.setState({title})
-  }
-
-  /*handleSubmit(event) {
-    event.preventDefault();
-    fetch(pets)
-      .then(response => response.json())
-      .then(data => {
-          console.log(data) //displays json data in console
-          this.props.onSubmit(data.pets);
-      }
-  }*/
-
   render () {
-    const { animals=[] } = this.context;
+    const { pets=[], types=[] } = this.context;
     return (
       <div className="search-db">
         <h2>Search the Pet DataBase</h2>
